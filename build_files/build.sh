5c9f215826fd1920da49bd990962aaf9987c1cb0
jahinzee <git@jahinzee.net>
2026-08-03 14:16:22 +1000

    build: copy and adapt from jahinzee/metis

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-# Copy the contents of system_files/ of the git repo to /
-cp -avf "/ctx/system_files"/. /
+rm /opt && mkdir /opt # PATCH: Recreate /opt as a real directory to allow Helium to install properly.
 
-### Install packages
+dnf5 copr enable -y imput/helium
+dnf5 remove -y firefox
+dnf5 install -y \
+	distrobox \
+	helium-bin \
+	nix \
+	nix-daemon \
+	podman
+dnf5 copr disable -y imput/helium
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux
-
-# Use a COPR Example:
+# Add /nix -> /var/nix mounts.
 #
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+cat <<EOF >/usr/lib/systemd/system/nix.mount
+[Unit]
+Description=Bind mount /var/nix to /nix
 
-#### Example for enabling a System Unit File
+[Mount]
+What=/var/nix
+Where=/nix
+Type=none
+Options=bind
 
-systemctl enable podman.socket
+[Install]
+WantedBy=local-fs.target
+EOF
+
+# Add default /nix bindings for new Distrobox containers.
+#
+mkdir -p /usr/share/distrobox
+cat <<EOF >/usr/share/distrobox/distrobox.conf
+container_manager="podman"
+container_additional_volumes="/nix:/nix:ro"
+EOF
+
+systemctl enable nix.mount
+systemctl enable nix-daemon.service
